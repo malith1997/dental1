42a5cedd052f419216a9cc8d6bf94b340f36d182
malith2cristal <malith.gayashan@cristalcode.net>
2023-03-17 19:14:27 +0530

    created 3rd page

---
+++ src/component/hero2.js
@@ -0,0 +1,23 @@
+import { Container } from '@chakra-ui/react'
+import React from 'react'
+
+export default function hero2() {
+  return (
+    <div>
+      <div className='container'>
+        <div className="card-11">
+        <h1 className='textcenter'>Advanced Dentistry</h1>
+        </div>
+
+        <div className="card-11">
+            <h1 className='textcenter'>High Quality Equipment</h1>
+        </div>
+
+        <div className="card-11">
+        <h1 className='textcenter'>Friendly Staff</h1>
+        </div>
+
+      </div>
+    </div>
+  )
+}
