7a19f69e1b65547deb388a0bf4427c1046f27e0f
malith2cristal <malith.gayashan@cristalcode.net>
2023-03-19 22:49:00 +0530

    add 3rd screen

---
+++ src/component/hero3.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { ButtonGroup,Divider,Stack,Image,Heading,Text,Button,Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+import Img5 from "../Img/BOOK1.png";
+import Img1 from "../Img/img5.png";
+import Img2 from "../Img/img6.png";
+// import ContactUs from "../component/sendemail"
+
+export default function Hero3() {
+  return (
+    <div>
+      <div className='row2'>
+                    <div className='card-05'> 
+                    <div className='container3'>
+                        <div className='row1'>
+                            <div className='card-20'>
+                                
+                                {/* <ContactUs /> */}
+                            </div>
+                                            
+                        </div>
+
+                        
+
+                       
+                              
+                        </div>
+                    </div>
+                    
+
+                    
+                    <div className='card-05'>
+                  
+
+                        <div className='row3'>
+                            <div className='card-04'>
+                            <Image className="img111" src={Img5} alt="img" />
+                            </div>
+                
+                        </div>
+
+
+                    </div>
+                   
+                </div>
+    </div>
+  )
+}
---
+++ src/component/sendemail.js
@@ -0,0 +1,62 @@
+
+import React, { useRef,useState } from 'react';
+import Alert from './Alert';
+import emailjs from '@emailjs/browser';
+import {
+    FormControl,
+    FormLabel,Input,
+    FormErrorMessage,
+    FormHelperText,
+  } from '@chakra-ui/react'
+
+
+const ContactUs = () => {
+
+  const [showAlert, setShowAlert] = useState(false);
+  const handleClick = () => {
+    // Perform action here
+    <Alert type="success" message="Successfully sent!" />
+  }
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_r5433yp', 'template_5iusaw4', form.current, '7hCSNowO3QBkPohay')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  
+    return (
+
+
+    <div>
+
+    <section className='formset'>
+      <form ref={form} onSubmit={sendEmail}>
+     
+        <input className='form1' type="text" name="user_name" placeholder='Name' /> 
+        
+        <input className='form2' type="email" name="user_email" placeholder='Email' />
+        
+        <textarea className='form3' name="message" placeholder='Message' />
+        <input className="BtnCard6" type="submit" value="Send" onClick={handleClick} />
+        alert("This is an alert message!");
+       
+      </form>
+
+      </section>
+
+
+    </div>
+
+        
+    );
+  };
+
+  export default ContactUs;
+
+
